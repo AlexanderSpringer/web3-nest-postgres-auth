30bb1ba049e391e6ca303b555dbb706d4be33268
Alexander Springer <33805769+AlexanderSpringer@users.noreply.github.com>
2023-07-27 17:37:40 +0200

    sync package

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS
   app.use(cookieParser());
-  await app.listen(process.env.REST_PORT ? '0.0.0.0:' + process.env.REST_PORT : '3000');
+  await app.listen(process.env.PORT || '3000');
 }
 bootstrap();
